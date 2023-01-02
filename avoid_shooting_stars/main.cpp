3a78308de7f48f02f726125a08951a69da8b7a66
hyeg <hyeg0121@naver.com>
2023-01-02 15:00:27 +0900

    게임 오버 기능 추가

--- avoid_shooting_stars/main.cpp
+++ avoid_shooting_stars/main.cpp
@@ -12,6 +12,7 @@ struct Player
 	Texture texture;
 	int speed = 3;
 	int width = 50, height = 50;
+	int life;
 };
 
 struct Shooting_stars {
@@ -20,6 +21,15 @@ struct Shooting_stars {
 	int speed = 5;
 	int width = 50, height = 50;
 	int respwan_time = 7;
+};
+
+int is_collide(RectangleShape obj1, RectangleShape obj2) {
+	if (obj1.getGlobalBounds().intersects(obj2.getGlobalBounds())) {
+		return 1;
+	}
+	else {
+		return 0;
+	}
 };
 
 const int W_WIDTH = 1000, W_HEIGHT = 1000;
@@ -36,6 +46,7 @@ int main(void)
 	long spent_time;
 	int score;
 	char info[100];
+	int is_gameover;
 
 	/* Font */
 	Font font;
@@ -46,19 +57,27 @@ int main(void)
 	text.setFont(font);
 	text.setPosition(10, 50);
 
+	/* GameOver */
+	RectangleShape gameover;
+	Texture go_t;
+	go_t.loadFromFile("./resources/images/gameover.png");
+	gameover.setSize(Vector2f(400, 400));
+	gameover.setTexture(&go_t);
+
 	/* Player */
 	Player player;
 	player.sprite.setFillColor(Color::White);
 	player.sprite.setSize(Vector2f(player.width, player.height));
 	player.sprite.setPosition(W_WIDTH / 2, W_HEIGHT * 0.8);
+	player.life = 3;
 
 	/* Shooting_stars */
 	struct Shooting_stars s_star; //sample
 	struct Shooting_stars stars[STARS_NUM];
 	for (int i = 0; i < STARS_NUM; i++) {
 		stars[i].sprite.setSize(Vector2f(s_star.width, s_star.height));
-		stars[i].speed = -(rand() % 7 + 1);
-		stars[i].sprite.setPosition(rand() % W_WIDTH-s_star.width, rand() % W_HEIGHT * 0.3);
+		stars[i].speed = -(rand() % 8 + 1);
+		stars[i].sprite.setPosition(rand() % W_WIDTH-s_star.width, rand() % W_HEIGHT * 0.4);
 	}
 
 	while (window.isOpen())
@@ -107,22 +126,40 @@ int main(void)
 		}
 
 		//老沥 矫埃 第 stars respwan
-		if (spent_time % (1000 * s_star.respwan_time) < 1000 / 60 + 1) {
-			for (int i = 0; i < STARS_NUM; i++) {
+		if (spent_time % (1000 * s_star.respwan_time) < 1000 / 60 + 1) 
+		{
+			for (int i = 0; i < STARS_NUM; i++) 
+			{
 				stars[i].sprite.setSize(Vector2f(s_star.width, s_star.height));
-				stars[i].speed = -(rand() % 7 + 1);
+				stars[i].speed = -(rand() % 8 + 1);
 				stars[i].sprite.setPosition(rand() % W_WIDTH - s_star.width, rand() % W_HEIGHT * 0.3);
+			}
+		}
+
+		//player客 面倒
+		for (int i = 0; i < STARS_NUM; i++) 
+		{
+			if (is_collide(player.sprite, stars[i].sprite)) {
+				score -= 100;
+				player.life--;
 			}
 		}
 
 		
 		window.clear(Color::Black);
 
-		window.draw(player.sprite);
-		window.draw(text);
-		for (int i = 0; i < STARS_NUM; i++)
-			window.draw(stars[i].sprite);
-
+		if (player.life < 0) {
+			gameover.setPosition(W_WIDTH / 2, W_HEIGHT / 2);
+			window.draw(gameover);
+		}
+		else 
+		{
+			window.draw(player.sprite);
+			window.draw(text);
+			for (int i = 0; i < STARS_NUM; i++)
+				window.draw(stars[i].sprite);
+		}
+		
 		window.display();
 	}
 
